6153f0d3562e7209b64a377094aa59f80dfc7938
Vadim Peretokin <vperetokin@hey.com>
2023-03-07 09:18:51 +0100

    Try running with Java 17

--- interversion/ci-run-validation.sh
+++ interversion/ci-run-validation.sh
@@ -10,7 +10,7 @@ tmp="/tmp/r4r5"
 rm -rf "$tmp"
 mkdir -p $tmp
 
-java -jar validator_cli.jar -version 5.0 ./r5/r4-2-r5/*.fml -ig ./r5/r4-2-r5 -alt-version R4 -output $tmp -output-style compact-split
+$JAVA_HOME_17_X64 -jar validator_cli.jar -version 5.0 ./r5/r4-2-r5/*.fml -ig ./r5/r4-2-r5 -alt-version R4 -output $tmp -output-style compact-split
 
 # first row is the header, second row is where the data starts
 row=2
