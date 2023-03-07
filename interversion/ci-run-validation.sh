364b22fd1d63885dcf49b27d38686ee47d2b24b5
Vadim Peretokin <vperetokin@hey.com>
2023-03-07 09:45:23 +0100

    Clarify locale setting

--- interversion/ci-run-validation.sh
+++ interversion/ci-run-validation.sh
@@ -10,6 +10,7 @@ tmp="/tmp/r4r5"
 rm -rf "$tmp"
 mkdir -p $tmp
 
+# set locale otherwise validator will stop on first error
 export LANG=en_US.UTF-8
 "$JAVA_HOME_17_X64/bin/java" -jar validator_cli.jar -version 5.0 ./r5/r4-2-r5/*.fml -ig ./r5/r4-2-r5 -alt-version R4 -output $tmp -output-style compact-split
 
